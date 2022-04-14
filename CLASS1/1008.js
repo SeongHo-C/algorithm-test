ab90c983074e3f50190f44bfd6183a8376bf83a8
SeongHo-C <leeseong010@naver.com>
2022-04-15 02:12:12 +0900

    20220415 class1.1546 평균

---
+++ CLASS1/1008.js
@@ -0,0 +1,6 @@
+// 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+var fs = require('fs');
+var input = fs.readFileSync('예제.txt').toString().split(' ');
+var a = parseFloat(input[0]);
+var b = parseFloat(input[1]);
+console.log(a / b);
