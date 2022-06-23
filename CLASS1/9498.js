113aac20c8829301cdfbc0184b3698fe9767e5fb
SeongHo-C <leeseong010@naver.com>
2022-06-23 20:50:28 +0900

    20220623 백준 10171, 10998, 9498번

---
+++ CLASS1/9498.js
@@ -0,0 +1,22 @@
+// 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+
+const input = require('fs').readFileSync('예제.txt').toString().trim();
+const grade = parseInt(input / 10);
+
+switch (grade) {
+  case 10:
+  case 9:
+    console.log('A');
+    break;
+  case 8:
+    console.log('B');
+    break;
+  case 7:
+    console.log('C');
+    break;
+  case 6:
+    console.log('D');
+    break;
+  default:
+    console.log('F');
+}
