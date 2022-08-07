0273deeb958befe782299d9ee67e2bfa237f3ccb
SeongHo-C <leeseong010@naver.com>
2022-08-07 17:23:12 +0900

    [백준] 2751번 수 정렬하기 2

---
+++ CLASS2/2751.py
@@ -0,0 +1,15 @@
+# N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+data = []
+
+for i in range(N):
+        data.append(int(input()))
+    
+# 오름차순 정렬  
+data.sort()
+
+print(*data, sep='\n')
+    
