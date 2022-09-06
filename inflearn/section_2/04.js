c12c4ca4051794116ab9ee4b488dc5ef69547f93
SeongHo-C <leeseong010@naver.com>
2022-09-06 22:46:19 +0900

    [인프런] 점수 계산

---
+++ inflearn/section_2/04.js
@@ -0,0 +1,19 @@
+// 점수 계산
+
+function solution(arr) {
+  let answer = 0,
+    cnt = 0;
+
+  for (let x of arr) {
+    if (x === 1) {
+      answer += ++cnt;
+    } else {
+      cnt = 0;
+    }
+  }
+
+  return answer;
+}
+
+let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+console.log(solution(arr));
