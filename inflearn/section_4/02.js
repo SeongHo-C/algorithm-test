743bc4951f98f9bc6443f40818c07155795a2ee7
SeongHo-C <leeseong010@naver.com>
2022-09-14 14:47:14 +0900

    [인프런] 뒤집은 소수

---
+++ inflearn/section_4/02.js
@@ -0,0 +1,32 @@
+// 뒤집은 소수
+
+function isPrime(num) {
+  if (num === 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(arr) {
+  let answer = [];
+
+  // arr.forEach((num) => {
+  //   let reverseNum = Number(num.toString().split('').reverse().join(''));
+  //   if (isPrime(reverseNum)) answer.push(reverseNum);
+  // });
+  for (let x of arr) {
+    let res = 0;
+    while (x) {
+      let t = x % 10;
+      res = res * 10 + t;
+      x = parseInt(x / 10);
+    }
+    if (isPrime(res)) answer.push(res);
+  }
+
+  return answer;
+}
+
+let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
+console.log(solution(arr));
