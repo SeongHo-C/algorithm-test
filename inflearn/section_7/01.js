05c0457dfa3c1106cdf4d346e55e4b07f022f7da
SeongHo-C <leeseong010@naver.com>
2022-09-29 14:28:02 +0900

    [인프런] 선택 정렬

---
+++ inflearn/section_7/01.js
@@ -0,0 +1,34 @@
+// 선택 정렬
+
+function solution(arr) {
+  let answer = arr;
+
+  //   for (let i = 0; i < arr.length; i++) {
+  //     let min = arr[i];
+  //     let index = i;
+
+  //     // 최솟값 찾기
+  //     for (let j = i + 1; j < arr.length; j++) {
+  //       if (min > arr[j]) {
+  //         min = arr[j];
+  //         index = j;
+  //       }
+  //     }
+
+  //     let temp = arr[i];
+  //     arr[i] = arr[index];
+  //     arr[index] = temp;
+  //   }
+  for (let i = 0; i < arr.length; i++) {
+    let idx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[idx]) idx = j;
+    }
+    [arr[i], arr[idx]] = [arr[idx], arr[i]];
+  }
+
+  return answer;
+}
+
+let arr = [13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
