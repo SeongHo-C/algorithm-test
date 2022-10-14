d43eeedf0ccf1d0432189e2926543228bcffc36c
SeongHo-C <leeseong010@naver.com>
2022-10-14 16:19:26 +0900

    [인프런] 이진수 출력(재귀)

---
+++ inflearn/section_8/02.js
@@ -0,0 +1,17 @@
+// 이진수 출력(재귀)
+
+function solution(n) {
+  let answer = '';
+
+  function DFS(L) {
+    if (L === 0) return;
+    else {
+      DFS(parseInt(L / 2));
+      answer += L % 2;
+    }
+  }
+  DFS(n);
+  return answer;
+}
+
+console.log(solution(11));
