f2b48e473ac848e927a0e9470338b629155082b3
seongho <leeseong7130@gmail.com>
2023-04-16 00:33:28 +0900

    Refactor: [백준] 17413번 단어 뒤집기 2
    stack = []를 사용하여 스택을 초기화할 경우 메모리를 낭비하게 되므로 stack.length = 0을 사용하여 더 효율적으로 메모리 관리

--- BOJ/STRING/17413.js
+++ BOJ/STRING/17413.js
@@ -4,28 +4,37 @@ const S = require('fs').readFileSync('input.txt').toString().trim();
 
 function solution(S) {
   let answer = '';
-  let stack = [];
+  const stack = [];
 
-  for (let i = 0; i < S.length; i++) {
-    if (S[i] === '<') {
+  for (let x of S) {
+    // 태그의 시작
+    if (x === '<') {
+      // answer 변수에 누적하지 않은 단어가 존재하는지 확인
       if (stack.length > 0) {
         answer += stack.reverse().join('');
-        stack = [];
+        // stack = []를 사용하여 스택을 초기화할 경우 메모리 낭비!!
+        stack.length = 0;
       }
       stack.push('<');
-    } else if (S[i] === '>') {
+    }
+    // 태그의 끝
+    else if (x === '>') {
       answer += stack.join('') + '>';
-      stack = [];
-    } else if (S[i] === ' ' && !stack.includes('<')) {
+      stack.length = 0;
+    }
+    // 연속하는 두 단어 사이의 공백이 있을 경우
+    else if (x === ' ' && !stack.includes('<')) {
       answer += stack.reverse().join('') + ' ';
-      stack = [];
+      stack.length = 0;
     } else {
-      stack.push(S[i]);
+      stack.push(x);
     }
   }
 
+  // 문자열에 태그 없이 단어만 존재하는 경우(최종 확인)
   if (stack.length > 0) answer += stack.reverse().join('');
-  return answer.trim();
+
+  return answer;
 }
 
 console.log(solution(S));
