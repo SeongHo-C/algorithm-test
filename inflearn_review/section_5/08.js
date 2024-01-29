a398c08a3959cb23ad06fa895ac726c158f22a4c
SeongHo-C <leeseong7130@gmail.com>
2024-01-29 12:58:07 +0900

    [인프런/섹션5] 8번 수정
    compareMaps 함수를 한 곳에서 호출이 가능하게 하여 코드의 복잡성을 줄임

--- inflearn_review/section_5/08.js
+++ inflearn_review/section_5/08.js
@@ -1,41 +1,39 @@
 // 모든 아나그램 찾기(해쉬, 투포인터, 슬라이딩 윈도우)
-function compareMap(sMap, tMap) {
-  let result = true;
-
+function compareMaps(sMap, tMap) {
   if (sMap.size !== tMap.size) return false;
   for (const [key, value] of sMap) {
     if (!tMap.has(key) || tMap.get(key) !== value) return false;
   }
-  console.log(result);
-  return result;
+
+  return true;
 }
 
 function solution(s, t) {
   let answer = 0;
+  const tLen = t.length;
   const sMap = new Map();
   const tMap = new Map();
-
-  for (let i = 0; i < t.length; i++) {
-    if (sMap.has(s[i])) sMap.set(s[i], sMap.get(s[i]) + 1);
-    else sMap.set(s[i], 1);
-  }
 
   for (const alpha of t) {
     if (tMap.has(alpha)) tMap.set(alpha, tMap.get(alpha) + 1);
     else tMap.set(alpha, 1);
   }
-  if (compareMap(sMap, tMap)) answer++;
+
+  for (let i = 0; i < tLen - 1; i++) {
+    if (sMap.has(s[i])) sMap.set(s[i], sMap.get(s[i]) + 1);
+    else sMap.set(s[i], 1);
+  }
 
   let lt = 0;
-  for (let rt = t.length; rt < s.length; rt++) {
+  for (let rt = tLen - 1; rt < s.length; rt++) {
     if (sMap.has(s[rt])) sMap.set(s[rt], sMap.get(s[rt]) + 1);
     else sMap.set(s[rt], 1);
+
+    if (compareMaps(sMap, tMap)) answer++;
 
     sMap.set(s[lt], sMap.get(s[lt]) - 1);
     if (sMap.get(s[lt]) === 0) sMap.delete(s[lt]);
     lt++;
-
-    if (compareMap(sMap, tMap)) answer++;
   }
 
   return answer;
