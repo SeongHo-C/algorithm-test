890a8b624793ac67e13839778c53d1b85305a2af
SeongHo-C <leeseong7130@gmail.com>
2024-02-22 13:35:45 +0900

    [인프런/섹션7] 2번 수정, 3번 복습

--- inflearn_review/section_7/03.js
+++ inflearn_review/section_7/03.js
@@ -1,15 +1,15 @@
-// 버블 정렬
+// Special Sort(구글 인터뷰)
 function solution(arr) {
   const answer = arr;
 
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - (i + 1); j++) {
-      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      if (arr[j] > 0 && arr[j + 1] < 0) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
     }
   }
 
   return answer.join(' ');
 }
 
-let arr = [13, 5, 11, 7, 23, 15];
+let arr = [1, 2, 3, -3, -2, 5, 6, -6];
 console.log(solution(arr));
