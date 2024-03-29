0ef518a647d6e0476acf8725a4ad4c57311e2869
SeongHo-C <leeseong7130@gmail.com>
2024-03-29 11:44:51 +0900

    [인프런/섹션7] 11번 복습

---
+++ inflearn_review/section_7/11.js
@@ -0,0 +1,38 @@
+// 뮤직비디오(결정알고리즘)
+function count(songs, capacity) {
+  let cnt = 1;
+  let sum = 0;
+
+  for (const song of songs) {
+    if (sum + song > capacity) {
+      cnt++;
+      sum = song;
+    } else {
+      sum += song;
+    }
+  }
+
+  return cnt;
+}
+
+function solution(m, songs) {
+  let answer;
+  const lt = Math.max(...songs); // 최소 용량
+  const rt = songs.reduce((a, b) => a + b); // 최대 용량
+
+  while (lt <= rt) {
+    const mid = Math.floor((lt + rt) / 2);
+
+    if (count(songs, mid) <= m) {
+      answer = mid;
+      rt = mid - 1;
+    } else {
+      lt = mid + 1;
+    }
+  }
+
+  return answer;
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(solution(3, arr));
