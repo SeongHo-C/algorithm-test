8704b039d1e45261ef189c6646691f23e5abe391
SeongHo-C <leeseong7130@gmail.com>
2024-05-16 00:31:34 +0900

    [프로그래머스/Lv. 2] 짝지어 제거하기
    효율성 향상, 조건 추가(문자열의 길이가 홀수일 경우)

--- programmers/Level 2/짝지어 제거하기.js
+++ programmers/Level 2/짝지어 제거하기.js
@@ -1,11 +1,13 @@
 function solution(s) {
   const stack = [];
 
-  for (const alpha of s) {
-    stack[stack.length - 1] === alpha ? stack.pop() : stack.push(alpha);
+  if (s.length % 2 !== 0) return 0;
+  // for...of 명령문에 비해 효율성 테스트에서 유리
+  for (let i = 0; i < s.length; i++) {
+    stack[stack.length - 1] === s[i] ? stack.pop() : stack.push(s[i]);
   }
 
-  return stack.length ? 0 : 1;
+  return stack.length === 0 ? 1 : 0;
 }
 
 console.log(solution('baabaa'));
