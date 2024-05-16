061dfcbae1ac53cfdfe45d04f4869789acc38b33
SeongHo-C <leeseong7130@gmail.com>
2024-05-16 09:29:48 +0900

    [프로그래머스/Lv. 2] 카펫

---
+++ programmers/Level 2/카펫.js
@@ -0,0 +1,12 @@
+function solution(brown, yellow) {
+  // 최소 세로 크기 → 3
+  for (let h = 3; h <= (brown + yellow) / h; h++) {
+    const w = (brown + yellow) / h;
+
+    if ((w - 2) * (h - 2) === yellow) return [w, h];
+  }
+}
+
+console.log(solution(10, 2));
+console.log(solution(8, 1));
+console.log(solution(24, 24));
