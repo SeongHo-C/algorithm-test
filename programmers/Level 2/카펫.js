c290a21e1533f30101c319abfdc6bc967e959e51
SeongHo-C <leeseong7130@gmail.com>
2024-05-16 10:11:05 +0900

    [프로그래머스/Lv. 2] 카펫
    for 문 조건식 수정

--- programmers/Level 2/카펫.js
+++ programmers/Level 2/카펫.js
@@ -1,6 +1,6 @@
 function solution(brown, yellow) {
   // 최소 세로 크기 → 3
-  for (let h = 3; h <= (brown + yellow) / h; h++) {
+  for (let h = 3; h <= yellow + 2; h++) {
     const w = (brown + yellow) / h;
 
     if ((w - 2) * (h - 2) === yellow) return [w, h];
