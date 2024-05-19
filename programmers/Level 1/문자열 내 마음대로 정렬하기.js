8a94c3edb74db476517c594650e9f5bef5885bab
SeongHo-C <leeseong7130@gmail.com>
2024-05-19 23:57:13 +0900

    [프로그래머스/Lv. 1] 문자열 내 마음대로 정렬하기

---
+++ programmers/Level 1/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,6 @@
+function solution(strings, n) {
+  return strings.sort().sort((a, b) => a[n].charCodeAt() - b[n].charCodeAt());
+}
+
+console.log(solution(['sun', 'bed', 'car'], 1));
+console.log(solution(['abce', 'abcd', 'cdx'], 2));
