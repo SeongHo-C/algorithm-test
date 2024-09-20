ef82949abdd96b9f0d43af5c84aadf10efb52b1d
SeongHo-C <leeseong7130@gmail.com>
2024-09-20 18:16:26 +0900

    [백준/CLASS 1] 2475번 검증수

---
+++ BOJ/CLASS 1/Python/2475.py
@@ -0,0 +1,15 @@
+# 검증수
+data = list(map(int, input().split()))
+
+"""
+# 내 풀이 방법
+total = 0
+
+for i in data:
+  total += i ** 2
+
+print(total % 10)
+"""
+
+# 리스트 컴프리헨션 사용
+print(sum(i ** 2 for i in data) % 10)
