64dadba0f1afbe00ff392b815d29a46143c057c1
SeongHo-C <leeseong7130@gmail.com>
2024-09-21 20:49:00 +0900

    [백준/CLASS 1] 2675번 문자열 반복

---
+++ BOJ/CLASS 1/Python/2675.py
@@ -0,0 +1,12 @@
+# 문자열 반복
+t = int(input())
+
+result = []
+
+for _ in range(t):
+  r, s = input().split()
+  r = int(r)
+
+  result.append(''.join(char * r for char in s))
+
+print('\n'.join(result))
