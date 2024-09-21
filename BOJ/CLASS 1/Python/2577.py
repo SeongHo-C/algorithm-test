54565673e59cceb0ac2725926b68735d8cea542c
SeongHo-C <leeseong7130@gmail.com>
2024-09-21 18:00:56 +0900

    [백준/CLASS 1] 2577번 숫자의 개수

---
+++ BOJ/CLASS 1/Python/2577.py
@@ -0,0 +1,13 @@
+# 숫자의 개수
+a, b, c = (int(input()) for _ in range(3))
+
+result = str(a * b * c)
+
+"""
+# 내 풀이 방법
+for i in range(10):
+  print(result.count(str(i)))
+"""
+
+# 리스트 컴프리헨션 사용
+print("\n".join(str(result.count(str(i))) for i in range(10)))
